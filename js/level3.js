9b4f7b3be7b319e703b53610bccba2eada5d5781
Kirill Ageev <kirill_ageev@epam.com>
2017-12-03 18:05:36 +0300

    Give boss less lives

--- js/level3.js
+++ js/level3.js
@@ -8,7 +8,7 @@ demo.level3.prototype = {
     currentLevel = 3
     bgm3 = game.add.audio('bgm3', 0.4, true)
     bgm3.play()
-    lives = 100
+    lives = 70
     var fragmentSrc = [
 
       'precision mediump float;',
