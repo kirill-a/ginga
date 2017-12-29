0b596397f8a4152d8731069f9204ad1335187369
Kirill Ageev <kirill_ageev@epam.com>
2017-12-29 20:45:34 +0300

    Fix gulp async del

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,33 @@ var $ = require('gulp-load-plugins')({lazy: true})
 var config = require('./gulp.config')()
 var del = require('del')
 
-gulp.task('lint', function() {
-    return gulp
-        .src(config.alljs)
+gulp.task('lint', () => {
+    return gulp.src(config.alljs)
         .pipe($.eslint(config.exclude))
         .pipe($.eslint.format())
 })
 
-gulp.task('clean', function() {
-    del(config.build)
+gulp.task('clean', () => {
+    del(config.build).then((paths) => {
+        console.log('Deleted files and folders:\n', paths.join('\n'))
+    })
 })
 
-gulp.task('build', ['clean'], function() {
-    return gulp
-        .src(config.list)
+gulp.task('build', ['clean'], () => {
+    return gulp.src(config.list)
         .pipe($.uglify())
         .pipe($.concat('min.js'))
         .pipe(gulp.dest(config.build))
+        
 })
 
-gulp.task('pkg', ['build'], function() {
-    return gulp
-        .src(config.pkg, { base: "." })
+gulp.task('copy', ['build', 'clean'], () => {
+    return gulp.src(config.pkg, { base: "." })
+        .pipe(gulp.dest(config.build))
+})
+
+gulp.task('default', ['clean', 'build', 'copy'], () => {
+    return gulp.src('build/*')
+        .pipe($.zip('build.zip'))
         .pipe(gulp.dest(config.build))
 })
